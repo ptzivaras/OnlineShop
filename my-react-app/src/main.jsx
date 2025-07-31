d732050f3b5994d42c2d119ace7374def3d542b6
ptzivaras <82588420+ptzivaras@users.noreply.github.com>
2025-07-31 22:40:03 +0300

    Debug

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,18 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes.jsx';
 import './index.css'
 import 'react-multi-carousel/lib/styles.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Shop from './Shop.jsx';
 
 
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 //     <RouterProvider router={router}>
 //       <Shop />
 //     </RouterProvider>
