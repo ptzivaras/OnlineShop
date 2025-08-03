6b9d0144860a029f130b1373c8eaf887992a9685
ptzivaras <82588420+ptzivaras@users.noreply.github.com>
2025-08-03 17:22:56 +0300

    list products

--- my-react-app/README.md
+++ my-react-app/README.md
@@ -1,4 +1,33 @@
 # React + Vite
+# 🛒 https://www.uk.gr clone  Fullstack  Project (In Progress)
+Features: product browsing, cart management, and order processing.
+
+- **Frontend**: React.js, Redux, Bootstrap, Axios
+- **Backend**: Spring Boot, Spring Security, Spring Data JPA
+- **Database**: PostgreSQL
+- **Authentication**: JWT (JSON Web Tokens)
+- **Payment Gateway**: (Optional integration with Stripe/PayPal)
+- **Build Tools**: Maven, Webpack
+
+### Frontend
+- [x] Set up React project structure
+- [x] Implement a Home Page with different sections and a Footer
+- [X] Create Mockup API Data (Content)
+- [X] Pages Navigation & Categories Page with Filters
+- [X] Product Detail Page
+- [ ] Shopping cart functionality
+- [ ] User authentication (sign-up, login, logout)
+- [ ] Checkout process
+- [ ] Responsive design improvements
+
+### Backend
+- [ ] Set up Spring Boot project
+- [ ] Implement basic product API
+- [ ] User authentication and authorization with JWT
+- [ ] Order processing and management
+- [ ] Integration with payment gateway
+- [ ] Admin dashboard for product/order management
+
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- my-react-app/src/pages/ProductListPage/ProductListPage.jsx
+++ my-react-app/src/pages/ProductListPage/ProductListPage.jsx
@@ -5,6 +5,7 @@ import Categories from '../../components/Filters/Categories.jsx';
 import PriceFilter from '../../components/Filters/PriceFilter.jsx';
 import ColorsFilter from '../../components/Filters/ColorsFilter.jsx';
 import SizeFilter from '../../components/Filters/SizeFilter.jsx';
+import ProductCard from './ProductCard';
 
 const categories = content?.categories;
 
@@ -14,6 +15,9 @@ const ProductListPage = ({categoryType}) => {
     return categories?.find((category)=> category.code === categoryType);
   },[categoryType]);
 
+  const productListItems = useMemo(()=>{
+    return content?.products?.filter((product)=> product?.category_id === categoryContent?.id );
+  },[categoryContent]);
 
   return (
     <div>
@@ -44,6 +48,11 @@ const ProductListPage = ({categoryType}) => {
             <div className='p-[15px]'>
             <p className='text-black text-lg'>{categoryContent?.description}</p>
                 {/* Products */}
+                <div className='pt-4 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-8 px-2'>
+                {productListItems?.map((item,index)=>(
+                  <ProductCard key={item?.category_id+"_"+index} {...item}/>
+                ))}
+                </div>
             </div>
 
         </div>
